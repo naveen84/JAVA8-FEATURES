ca64c1f4e5eb2af2d173569fde52010617b88332
naveen84 <naveenachari39@gmail.com>
2019-08-24 22:18:07 +0530

    this is example of intpredicate

---
+++ src/com/naveen/IntPredicateDemo.java
@@ -0,0 +1,19 @@
+package com.naveen;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntPredicate;
+
+public class IntPredicateDemo {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(2,3,4,8,10);
+		IntPredicate isEvenFunction = num -> num%2==0;
+		for(int number : numbers){
+			if(isEvenFunction.test(number)){
+				System.out.println("EVEN NUMBERS ARE : "+number);
+			}
+		}
+	}
+
+}
